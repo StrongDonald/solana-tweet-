10f6b8f9d3fab9a7bd4d44606c124b01cff26054
StrongDonald <topdev19991228@gmail.com>
2022-07-22 08:31:28 -0700

    complete project

--- programs/solana-tweet/src/lib.rs
+++ programs/solana-tweet/src/lib.rs
@@ -1,15 +1,116 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6bFCwUtVEv2P1wbe7Xozgi9ooyauXEQWmy6DnCjrUVNv");
 
 #[program]
 pub mod solana_tweet {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn setup_platform(ctx: Context<TweetPlatform>) -> Result<()> {
+        let tweet = &mut ctx.accounts.tweet;
+
+        tweet.likes = 0;
+        tweet.message = ("").to_string();
+
+        Ok(())
+    }
+
+    pub fn write_tweet(
+        ctx: Context<WriteTweet>,
+        message: String,
+        user_public_key: Pubkey
+    ) -> Result<()> {
+        let tweet = &mut ctx.accounts.tweet;
+
+        if !tweet.message.trim().is_empty() {
+            return err!(Errors::CannotUpdateTweet);
+        }
+
+        if message.trim().is_empty() {
+            return err!(Errors::EmptyMessage);
+        }
+
+        tweet.message = message;
+
+        tweet.likes = 0;
+        tweet.creator = user_public_key;
+
+        Ok(())
+    }
+
+    pub fn like_tweet(
+        ctx: Context<LikeTweet>, 
+        user_liking_tweet: Pubkey,
+    ) -> Result<()> {
+        let tweet = &mut ctx.accounts.tweet;
+
+        if tweet.message.trim().is_empty() {
+            return err!(Errors::NotValidLike);
+        }
+
+        if tweet.likes == 5 {
+            return err!(Errors::ReachedMaxLikes);
+        }
+
+        let mut iter = tweet.people_who_liked.iter();
+
+        if iter.any(|&v| v == user_liking_tweet) {
+            return err!(Errors::UserLikedTweet);
+        }
+
+        tweet.likes += 1;
+        tweet.people_who_liked.push(user_liking_tweet);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TweetPlatform<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub tweet: Account<'info, Tweet>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WriteTweet<'info> {
+    #[account(mut)]
+    pub tweet: Account<'info, Tweet>,
+}
+
+#[derive(Accounts)]
+pub struct LikeTweet<'info> {
+    #[account(mut)]
+    pub tweet: Account<'info, Tweet>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Tweet {
+    message: String,
+    likes: u8,
+    creator: Pubkey,
+    people_who_liked: Vec<Pubkey>,
+}
+
+#[error_code]
+pub enum Errors {
+    #[msg("Tweet message cannot be updated")]
+    CannotUpdateTweet,
+
+    #[msg("Message cannot be empty")]
+    EmptyMessage,
+
+    #[msg("Cannot receive more thatn 5 likes")]
+    ReachedMaxLikes,
+
+    #[msg("Cannot like a tweet without a valid message")]
+    NotValidLike,
+
+    #[msg("User has already liked the tweet")]
+    UserLikedTweet,
+}
--- tests/solana-tweet.ts
+++ tests/solana-tweet.ts
@@ -1,16 +1,61 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, Wallet } from "@project-serum/anchor";
 import { SolanaTweet } from "../target/types/solana_tweet";
+import { expect, assert } from 'chai';
 
 describe("solana-tweet", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  const wallet = provider.wallet as Wallet;
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaTweet as Program<SolanaTweet>;
 
-  it("Is initialized!", async () => {
+  it("setup tweet platform", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tweetKeypair = anchor.web3.Keypair.generate();
+    
+    await program.rpc.setupPlatform({
+      accounts : {
+        tweet: tweetKeypair.publicKey,
+        user: wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweetKeypair]
+    });
+
+    let tweet = await program.account.tweet.fetch(tweetKeypair.publicKey);
+    
+    expect(tweet.likes).to.equal(0);
+    expect(tweet.message).to.equal('');
+  });
+
+  it('write a tweet', async () => {
+    const tweetKeypair = anchor.web3.Keypair.generate();
+
+    await program.rpc.setupPlatform({
+      accounts : {
+        tweet: tweetKeypair.publicKey,
+        user: wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweetKeypair]
+    });
+
+    await program.rpc.writeTweet(
+      'Hello World!',
+      wallet.publicKey, {
+        accounts: {
+          tweet: tweetKeypair.publicKey,
+        },
+        signers: [],
+      }
+    );
+
+    let tweet = await program.account.tweet.fetch(tweetKeypair.publicKey);
+
+    expect(tweet.likes).to.equal(0);
+    expect(tweet.message).to.equal('Hello World!');
+    expect(tweet.creator.toString()).to.equal(wallet.publicKey.toString());
   });
 });
